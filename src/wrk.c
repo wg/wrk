e297065a15fd44a8d7108bc5d740f9c47b8dd25f
Povilas Balciunas <balciunas90@gmail.com>
2015-09-22 08:23:19 +0300

    Update wrk to use extracted and tested parse_args

--- src/wrk.c
+++ src/wrk.c
@@ -4,6 +4,7 @@
 #include "script.h"
 #include "main.h"
 #include "config.h"
+#include "cli_options.h"
 
 static struct config cfg;
 
@@ -449,83 +450,6 @@ static char *copy_url_part(char *url, struct http_parser_url *parts, enum http_p
     }
 
     return part;
-}
-
-static struct option longopts[] = {
-    { "connections", required_argument, NULL, 'c' },
-    { "duration",    required_argument, NULL, 'd' },
-    { "threads",     required_argument, NULL, 't' },
-    { "script",      required_argument, NULL, 's' },
-    { "header",      required_argument, NULL, 'H' },
-    { "latency",     no_argument,       NULL, 'L' },
-    { "timeout",     required_argument, NULL, 'T' },
-    { "help",        no_argument,       NULL, 'h' },
-    { "version",     no_argument,       NULL, 'v' },
-    { NULL,          0,                 NULL,  0  }
-};
-
-static int parse_args(struct config *cfg, char **url, struct http_parser_url *parts, char **headers, int argc, char **argv) {
-    char **header = headers;
-    int c;
-
-    memset(cfg, 0, sizeof(struct config));
-    cfg->threads     = 2;
-    cfg->connections = 10;
-    cfg->duration    = 10;
-    cfg->timeout     = SOCKET_TIMEOUT_MS;
-
-    while ((c = getopt_long(argc, argv, "t:c:d:s:H:T:Lrv?", longopts, NULL)) != -1) {
-        switch (c) {
-            case 't':
-                if (scan_metric(optarg, &cfg->threads)) return -1;
-                break;
-            case 'c':
-                if (scan_metric(optarg, &cfg->connections)) return -1;
-                break;
-            case 'd':
-                if (scan_time(optarg, &cfg->duration)) return -1;
-                break;
-            case 's':
-                cfg->script = optarg;
-                break;
-            case 'H':
-                *header++ = optarg;
-                break;
-            case 'L':
-                cfg->latency = true;
-                break;
-            case 'T':
-                if (scan_time(optarg, &cfg->timeout)) return -1;
-                cfg->timeout *= 1000;
-                break;
-            case 'v':
-                printf("wrk %s [%s] ", VERSION, aeGetApiName());
-                printf("Copyright (C) 2012 Will Glozer\n");
-                break;
-            case 'h':
-            case '?':
-            case ':':
-            default:
-                return -1;
-        }
-    }
-
-    if (optind == argc || !cfg->threads || !cfg->duration) return -1;
-
-    if (!script_parse_url(argv[optind], parts)) {
-        fprintf(stderr, "invalid URL: %s\n", argv[optind]);
-        return -1;
-    }
-
-    if (!cfg->connections || cfg->connections < cfg->threads) {
-        fprintf(stderr, "number of connections must be >= threads\n");
-        return -1;
-    }
-
-    *url    = argv[optind];
-    *header = NULL;
-
-    return 0;
 }
 
 static void print_stats_header() {
