d9985cbabd28caec28d2abf8dda2895befae16dd
Rob McColl <rmccoll@ionicsecurity.com>
2017-10-04 14:12:33 -0400

    ssl: adding options for CA and client key / certificates

--- src/ssl.c
+++ src/ssl.c
@@ -8,7 +8,8 @@
 
 #include "ssl.h"
 
-SSL_CTX *ssl_init() {
+SSL_CTX *ssl_init(char *clientcert, char *clientkey,
+      char *cafile, char *capath) {
     SSL_CTX *ctx = NULL;
 
     SSL_load_error_strings();
@@ -16,10 +17,26 @@ SSL_CTX *ssl_init() {
     OpenSSL_add_all_algorithms();
 
     if ((ctx = SSL_CTX_new(SSLv23_client_method()))) {
-        SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL);
-        SSL_CTX_set_verify_depth(ctx, 0);
+        if (!cafile && !capath) {
+            SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL);
+            SSL_CTX_set_verify_depth(ctx, 0);
+        } else {
+            SSL_CTX_load_verify_locations(ctx, cafile, capath);
+        }
         SSL_CTX_set_mode(ctx, SSL_MODE_AUTO_RETRY);
         SSL_CTX_set_session_cache_mode(ctx, SSL_SESS_CACHE_CLIENT);
+
+        if (clientcert) {
+            if(1 != SSL_CTX_use_certificate_chain_file(ctx, clientcert)) {
+                fprintf(stderr, "unable to load client certificate chain\n");
+                return NULL;
+            }
+            if(1 != SSL_CTX_use_PrivateKey_file(
+                  ctx, clientkey, SSL_FILETYPE_PEM)) {
+                fprintf(stderr, "unable to load client key\n");
+                return NULL;
+            }
+        }
     }
 
     return ctx;
@@ -30,10 +47,13 @@ status ssl_connect(connection *c, char *host) {
     SSL_set_fd(c->ssl, c->fd);
     SSL_set_tlsext_host_name(c->ssl, host);
     if ((r = SSL_connect(c->ssl)) != 1) {
-        switch (SSL_get_error(c->ssl, r)) {
+        int e = 0;
+        switch (e = SSL_get_error(c->ssl, r)) {
             case SSL_ERROR_WANT_READ:  return RETRY;
             case SSL_ERROR_WANT_WRITE: return RETRY;
-            default:                   return ERROR;
+            default: {
+              fprintf(stderr, "ssl error %d\n", e);
+            } return ERROR;
         }
     }
     return OK;
@@ -48,10 +68,13 @@ status ssl_close(connection *c) {
 status ssl_read(connection *c, size_t *n) {
     int r;
     if ((r = SSL_read(c->ssl, c->buf, sizeof(c->buf))) <= 0) {
-        switch (SSL_get_error(c->ssl, r)) {
+        int e = 0;
+        switch (e = SSL_get_error(c->ssl, r)) {
             case SSL_ERROR_WANT_READ:  return RETRY;
             case SSL_ERROR_WANT_WRITE: return RETRY;
-            default:                   return ERROR;
+            default: {
+              fprintf(stderr, "ssl error %d\n", e);
+            } return ERROR;
         }
     }
     *n = (size_t) r;
@@ -61,10 +84,13 @@ status ssl_read(connection *c, size_t *n) {
 status ssl_write(connection *c, char *buf, size_t len, size_t *n) {
     int r;
     if ((r = SSL_write(c->ssl, buf, len)) <= 0) {
-        switch (SSL_get_error(c->ssl, r)) {
+        int e = 0;
+        switch (e = SSL_get_error(c->ssl, r)) {
             case SSL_ERROR_WANT_READ:  return RETRY;
             case SSL_ERROR_WANT_WRITE: return RETRY;
-            default:                   return ERROR;
+            default: {
+              fprintf(stderr, "ssl error %d\n", e);
+            } return ERROR;
         }
     }
     *n = (size_t) r;
